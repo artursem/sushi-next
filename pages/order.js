4027fdc70f04d6d87d39f22423c92cdbb99bccba
artursem <artur.semeniuk@gmail.com>
2022-01-17 21:01:09 +0100

    fetch menu from db

--- pages/order.js
+++ pages/order.js
@@ -1,12 +1,31 @@
-import { menu } from '../components/sushi/items';
+// import { menu } from '../components/sushi/items';
 import Menu from '../components/sushi/Menu';
 
-const Order = () => {
+const Order = (props) => {
 	return (
 		<main>
-			<Menu menu={menu} />
+			<Menu menu={props.menu} />
 		</main>
 	);
 };
 
 export default Order;
+
+export async function getStaticProps() {
+	const res = await fetch(
+		'https://sushi-e3a8a-default-rtdb.europe-west1.firebasedatabase.app/menu.json'
+	);
+	const data = await res.json();
+	return {
+		props: {
+			menu: data.map((item) => {
+				return {
+					id: item.id,
+					name: item.name,
+					price: item.price,
+					vege: item.vege,
+				};
+			}),
+		},
+	};
+}
