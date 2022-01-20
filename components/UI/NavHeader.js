3d3053b711eb142d2e9533728b59f592f67cb74a
artursem <artur.semeniuk@gmail.com>
2022-01-20 20:44:18 +0100

    add mobile menu

--- components/UI/NavHeader.js
+++ components/UI/NavHeader.js
@@ -1,24 +1,41 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import classes from './NavHeader.module.css';
+import { useState } from 'react';
 import Logo from './icons/Logo';
+import GenericButton from './GenericButton';
+import MenuIcon from './icons/MenuIcon';
+import CloseIcon from './icons/CloseIcon';
+import classes from './NavHeader.module.css';
 const NavHeader = () => {
 	const router = useRouter();
+	const [menuOpen, setMenuOpen] = useState(false);
+	const toggleMenuOpen = () => {
+		setMenuOpen((prevState) => !prevState);
+		console.log(menuOpen);
+	};
+	const closeMenu = () => {
+		setMenuOpen(false);
+	};
+
+	const linksStyle = `${classes.links} ${menuOpen ? classes.show : ''}`;
 	return (
 		<header className={classes.header}>
 			<span className={classes.logo}>
 				<Link href='/'>
-					<a>
+					<a onClick={closeMenu}>
 						<Logo />
 						<h1 className={classes.title}>omakase</h1>
 					</a>
 				</Link>
 			</span>
 			<nav className={classes.nav}>
-				<ul className={classes.links}>
+				<ul className={linksStyle}>
 					<li>
 						<Link href='/'>
-							<a className={router.pathname === '/' ? `${classes.active}` : ``}>
+							<a
+								onClick={closeMenu}
+								className={router.pathname === '/' ? `${classes.active}` : ``}
+							>
 								Home
 							</a>
 						</Link>
@@ -26,6 +43,7 @@ const NavHeader = () => {
 					<li>
 						<Link href='/order'>
 							<a
+								onClick={closeMenu}
 								className={
 									router.pathname === '/order' ? `${classes.active}` : ``
 								}
@@ -37,6 +55,7 @@ const NavHeader = () => {
 					<li>
 						<Link href='/reservations'>
 							<a
+								onClick={closeMenu}
 								className={
 									router.pathname === '/reservations' ? `${classes.active}` : ``
 								}
@@ -46,6 +65,11 @@ const NavHeader = () => {
 						</Link>
 					</li>
 				</ul>
+				<div className={classes.menuBtn}>
+					<GenericButton onClick={toggleMenuOpen}>
+						{menuOpen ? <CloseIcon /> : <MenuIcon />}
+					</GenericButton>
+				</div>
 			</nav>
 		</header>
 	);
--- components/UI/icons/MenuIcon.js
+++ components/UI/icons/MenuIcon.js
@@ -3,7 +3,7 @@ const MenuIcon = () => {
 		<svg
 			width='24'
 			height='24'
-			stroke-width='1.5'
+			strokeWidth='1.5'
 			viewBox='0 0 24 24'
 			fill='none'
 			xmlns='http://www.w3.org/2000/svg'
