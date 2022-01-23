2d60a8af9e3a26c6ff282018085ee8a86c92bc56
artursem <artur.semeniuk@gmail.com>
2022-01-23 13:27:25 +0100

    replace bg images with Image

--- components/sushi/MenuItem.js
+++ components/sushi/MenuItem.js
@@ -1,8 +1,11 @@
 import { useDispatch } from 'react-redux';
 import { cartActions } from '../../store/cartSlice';
+import Image from 'next/image';
 import Leaf from '../UI/icons/Leaf';
 import Button from '../UI/Button';
 import classes from './MenuItem.module.css';
+
+// import testSushi from '../../public/sushi/00.jpg'
 
 const MenuItem = ({ item }) => {
 	const { id, name, price, vege } = item;
@@ -21,13 +24,8 @@ const MenuItem = ({ item }) => {
 
 	return (
 		<section className={classes.section}>
-			<div
-				className={classes.foto}
-				style={{
-					backgroundImage: `url('/sushi/${id}.jpg')`,
-				}}
-			>
-				foto
+			<div className={classes.foto}>
+				<Image src={`/sushi/${id}.jpg`} width='250' height='200' alt={name} />
 			</div>
 			<div className={classes.name}>
 				{name}
